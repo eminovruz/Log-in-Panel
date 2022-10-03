76aa6f11c551fbd2cabf8286c59f0ffc714ffc89
Novr_wz05 <Novr_wz05@STHQ012E-10>
2022-10-03 08:51:27 +0400

    Add project files.

---
+++ Log in Panel/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log_in_Panel
+{
+
+
+    public class User
+    {
+        public string? name;
+        public string? surname;
+        public int age;
+        public string? fatherName;
+        public string? motherName;
+        public string? gender;
+        public string? county;
+        public DateTime? birthDay;
+
+
+        public User()
+        { }
+
+    }
+}
